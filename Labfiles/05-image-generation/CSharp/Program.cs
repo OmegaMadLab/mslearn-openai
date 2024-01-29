30301b9fdb8f5612df22000c7ec87b3a092c9a94
Marco Obinu <marco.obinu@omegamadlab.com>
2024-01-29 14:07:01 +0100

    Dall-e c#

--- Labfiles/05-image-generation/CSharp/Program.cs
+++ Labfiles/05-image-generation/CSharp/Program.cs
@@ -6,79 +6,61 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Web;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
 using System.Threading.Tasks;
+
+using Azure;
+
+// Add Azure OpenAI package
+using Azure.AI.OpenAI;
 
 namespace generate_image
 {
     class Program
     {
-        private static string? aoaiEndpoint;
-        private static string? aoaiKey;
         static async Task Main(string[] args)
         {
             try
             {
                 // Get config settings from AppSettings
-                IConfigurationBuilder builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
-                IConfigurationRoot configuration = builder.Build();
-                aoaiEndpoint = configuration["AzureOAIEndpoint"] ?? "";
-                aoaiKey = configuration["AzureOAIKey"] ?? "";
+                IConfiguration config = new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+                string? oaiEndpoint = config["AzureOAIEndpoint"];
+                string? oaiKey = config["AzureOAIKey"];
+                string? oaiModelName = config["AzureOAIModelName"];
+
+                if(string.IsNullOrEmpty(oaiEndpoint) || string.IsNullOrEmpty(oaiKey) || string.IsNullOrEmpty(oaiModelName))
+                {
+                    Console.WriteLine("Please check your appsettings.json file for missing or incorrect values.");
+                    return;
+                }
 
                 // Get prompt for image to be generated
                 Console.Clear();
-                string prompt = "";
                 Console.WriteLine("Enter a prompt to request an image:");
-                string prompt = Console.ReadLine() ?? "";
+                string prompt = Console.ReadLine();
 
-                // Make the initial call to start the job
-                using (var client = new HttpClient())
+                OpenAIClient client = new OpenAIClient(new Uri(oaiEndpoint), new AzureKeyCredential(oaiKey));
+
+                ImageGenerationOptions imgOptions = new ImageGenerationOptions()
                 {
-                    var contentType = new MediaTypeWithQualityHeaderValue("application/json");
-                    var api = "openai/images/generations:submit?api-version=2023-06-01-preview";
-                    client.BaseAddress = new Uri(aoaiEndpoint);
-                    client.DefaultRequestHeaders.Accept.Add(contentType);
-                    client.DefaultRequestHeaders.Add("api-key", aoaiKey);
-                    var data = new
-                    {
-                        prompt=prompt,
-                        n=1,
-                        size="512x512"
-                    };
+                    DeploymentName = oaiModelName,
+                    Size = "1024x1024",
+                    ImageCount = 1,
+                    Prompt = prompt,
+                    Style = "vivid",
+                    Quality = "standard"
+                };
 
-                    var jsonData = JsonSerializer.Serialize(data);
-                    var contentData = new StringContent(jsonData, Encoding.UTF8, "application/json");
-                    var init_response = await client.PostAsync(api, contentData); 
-
-                    // Get the operation-location URL for the callback
-                    var callback_url = init_response.Headers.GetValues("operation-location").FirstOrDefault();
-
-                    // Poll the callback URL until the job has succeeeded (or 100 attempts)
-                    var response = await client.GetAsync(callback_url); 
-                    var stringResponse = await response.Content.ReadAsStringAsync();
-                    var status = JsonSerializer.Deserialize<Dictionary<string,object>>(stringResponse)["status"];
-                    var tries = 1;
-                    while (status.ToString() != "succeeded" && tries < 101)
-                    {
-                        Thread.Sleep (3000); // wait 3 seconds to avoid rate limit
-                        tries ++;
-                        response = await client.GetAsync(callback_url);
-                        stringResponse = await response.Content.ReadAsStringAsync();
-                        status = JsonSerializer.Deserialize<Dictionary<string,object>>(stringResponse)["status"];
-                        Console.WriteLine(tries.ToString() + ": " + status);
-                    }
-
-                    // Get the results
-                    stringResponse = await response.Content.ReadAsStringAsync();
-                    JsonNode contentNode = JsonNode.Parse(stringResponse)!;
-                    JsonNode resultNode = contentNode!["result"];
-                    JsonNode dataNode = resultNode!["data"];
-                    JsonNode urlNode = dataNode[0]!;
-                    JsonNode url = urlNode!["url"];
-
-                    // Display the URL for the generated image
-                    Console.WriteLine(url.ToJsonString().Replace(@"\u0026", "&"));
-
-                }
+                ImageGenerations img = client.GetImageGenerations(imgOptions);
+                Console.WriteLine("Response from Dall-e 3");
+                Console.WriteLine("Revised prompt:");
+                Console.WriteLine(img.Data[0].RevisedPrompt);
+                Console.WriteLine("Image url:");
+                Console.WriteLine(img.Data[0].Url.ToString());
+                
+                
             }
             catch (Exception ex)
             {
